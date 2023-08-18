df3b7c2ed501efc3f920da53403998143440d2e6
Fayed <aa.abdo20000@gmail.com>
2023-08-18 10:24:48 +0300

    Ex1

---
+++ First Term/unit2/Lesson3_C_Basics/Assignments/Homework1/main.c
@@ -0,0 +1,27 @@
+/*
+ * main.c
+ *
+ *  Created on: 15 Aug 2023
+ *      Author: mnj
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int i, j;
+
+	while(i=0; i<=9; i++)
+	{
+		while(j=i; j<=9; j++)
+		{
+			printf("%d", j);
+		}
+		printf("\n");
+	}
+}
+
+
+
