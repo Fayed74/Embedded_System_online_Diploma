f8d3bfe23002297154af0b14d73441da819aa615
Fayed <aa.abdo20000@gmail.com>
2023-08-19 20:04:23 +0300

    solving HW1 EX5

---
+++ First_Term/unit2/Lesson3_C_Basics/Assignments/Homework1/EX5/main.c
@@ -0,0 +1,22 @@
+/*
+ * EX5
+ *
+ *  Created on: 19 Aug 2023
+ *      Author: ABDEL RAHMAN FAID
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	char x;
+	printf("Enter a character: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%c", &x);
+	printf("ASCII value of %c = %d", x, x);
+}
+
+
+
