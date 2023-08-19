f94c0d0dbd2385364c2048102206e5a17bda1944
Fayed <aa.abdo20000@gmail.com>
2023-08-19 20:01:00 +0300

    solving HW1 EX2

---
+++ First_Term/unit2/Lesson3_C_Basics/Assignments/Homework1/EX2/main.c
@@ -0,0 +1,22 @@
+/*
+ * EX2
+ *
+ *  Created on: 19 Aug 2023
+ *      Author: ABDEL RAHMAN FAID
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int N;
+	printf("Enter an integer: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d", &N);
+	printf("You entered: %d", N);
+}
+
+
+
