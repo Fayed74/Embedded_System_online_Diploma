8edbdf6ce34f9f58af39772e1ec7c60c67f15204
Fayed <aa.abdo20000@gmail.com>
2023-08-19 20:03:25 +0300

    solving HW1 EX4

---
+++ First_Term/unit2/Lesson3_C_Basics/Assignments/Homework1/EX4/main.c
@@ -0,0 +1,22 @@
+/*
+ * EX4
+ *
+ *  Created on: 19 Aug 2023
+ *      Author: ABDEL RAHMAN FAID
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	float N1, N2;
+	printf("Enter two numbers: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%f %f", &N1, &N2);
+	printf("Product: %f", N1*N2);
+}
+
+
+
