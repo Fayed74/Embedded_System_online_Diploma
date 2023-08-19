fa5bf150e7d2889da221be1c7e1d0d14e8a2f6e7
Fayed <aa.abdo20000@gmail.com>
2023-08-19 19:57:02 +0300

    solving HW1 Ex1

---
+++ First_Term/unit2/Lesson3_C_Basics/Assignments/Homework1/EX1/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: 19 Aug 2023
+ *      Author: ABDEL RAHMAN FAID
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	printf("C Programming");
+}
+
+
+
