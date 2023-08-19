2b832ee3885bf8e4c75f26bce54033005b1dc3b0
Fayed <aa.abdo20000@gmail.com>
2023-08-19 20:02:31 +0300

    solving HW1 EX3

---
+++ First_Term/unit2/Lesson3_C_Basics/Assignments/Homework1/EX3/main.c
@@ -0,0 +1,22 @@
+/*
+ * EX3
+ *
+ *  Created on: 19 Aug 2023
+ *      Author: ABDEL RAHMAN FAID
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int N1, N2;
+	printf("Enter two integers: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d %d", &N1, &N2);
+	printf("Sum: %d", N1+N2);
+}
+
+
+
