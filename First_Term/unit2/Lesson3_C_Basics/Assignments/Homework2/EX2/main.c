c229a7fd17229b374d9ea884f403026fa7b91b40
Fayed <aa.abdo20000@gmail.com>
2023-08-20 20:27:54 +0300

    sloving HW2 EX2

---
+++ First_Term/unit2/Lesson3_C_Basics/Assignments/Homework2/EX2/main.c
@@ -0,0 +1,41 @@
+/*
+ * HW2 EX2
+ *
+ *  Created on: 20 Aug 2023
+ *      Author: ABDEL RAHMAN FAID
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char alphabet, vowels[] = {'a', 'e', 'i', 'o', 'u'};
+	int numVowels, isvowel = 0;
+
+	numVowels = sizeof(vowels)/sizeof(vowels[0]);
+	printf("Enter an alphabet: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%c", &alphabet);
+	for(int i=0; i<numVowels; i++)
+	{
+		if(alphabet == vowels[i])
+		{
+			isvowel = 1;
+			break;
+		}
+	}
+
+	if(isvowel)
+	{
+		printf("%c is a vowel", alphabet);
+	}
+	else
+	{
+		printf("%c is a consonant", alphabet);
+	}
+
+	return 0;
+}
+
+
+
