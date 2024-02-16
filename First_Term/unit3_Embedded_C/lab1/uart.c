5181af4458140eaf5cc796ec0b4c691b9b0b9df1
Fayed <aa.abdo20000@gmail.com>
2024-02-17 01:11:46 +0800

    unit3 lab1

---
+++ First_Term/unit3_Embedded_C/lab1/uart.c
@@ -0,0 +1,13 @@
+#include "stdint.h"
+#include "Platform_Types.h"
+
+#define UART0 *(vuint8_t*)((uint8_t*)0x101f1000)
+
+void uart_send(char* p_message)
+{
+	while(*p_message != '\0')
+	{
+		UART0 = (uint8_t)(*p_message);
+		p_message++;
+	}
+}
---
+++ First_Term/unit3_Embedded_C/lab1/uart.h
@@ -0,0 +1,5 @@
+#ifndef UART_H
+#define UART_H
+void uart_send(char* p_message);
+#endif
+
